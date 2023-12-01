21b85d2036344ccc42c2393e3e54f62d3f6ab594
Gordon Erlebacher <gerlebacher@class108.sc.fsu.edu>
2023-11-30 20:17:16 -0500

    Added comments and timing to HF_zephyr.py

--- zephyr_experiments/HF_zephyr.py
+++ zephyr_experiments/HF_zephyr.py
@@ -1,16 +1,23 @@
 # Author: G. Erlebacher
 # The model is run on Hugginface, but not using Langchain
 
+# model is running on the GPU
+
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers import GenerationConfig
+from time import time
+#import torch
+#print(torch.cuda.is_available())  # CUDA is available
+#quit()
 
 class HuggingFacePipeline:
     def __init__(self, model, tokenizer):
         self.model = model
         self.tokenizer = tokenizer
 
+    # trust_remote_code=False (mac), True (classroom)
     @classmethod
-    def from_model_id(cls, model_id, task, trust_remote_code=False, model_kwargs=None):
+    def from_model_id(cls, model_id, task, trust_remote_code=True, model_kwargs=None):
         if model_kwargs is None:
             model_kwargs = {}
 
@@ -30,6 +37,7 @@ class HuggingFacePipeline:
 #----------------------------------------------------------------------
 if __name__ == "__main__":
     # Usage
+    start = time()
     llm = HuggingFacePipeline.from_model_id(
         #model_id="NousResearch/Yarn-Mistral-7b-64k",
         model_id="HuggingFaceH4/zephyr-7b-beta",
@@ -37,13 +45,17 @@ if __name__ == "__main__":
         trust_remote_code=True,
         model_kwargs={"max_length": 50}
     )
+    print(f"time to load the model: {time() - start} sec")
 
 
     # Define a prompt for the model
     prompt = "Please explain how I could travel faster arbitrarily close to the speed of light?"
 
     temperature = 0.1
+
     for i in range(3):
+        start = time()
         prompt = "Are you happy?"   # Cannot use a list
         text = llm.generate_text(prompt, temperature=temperature)
+        print(f"time for inference: {time()-start} sec")
         print(text)
--- zephyr_experiments/README.md
+++ zephyr_experiments/README.md
@@ -1,7 +1,10 @@
 # Zephyr
 I am running the pretrained version of Zephyr on my CPU on the Mac (not the GPU). Here are first results.
 What is amazing is how close the structure of these sentences are to one another.
-The code took about 102 seconds!!!
+The code took about 102 seconds on my mac!!! Took 928s on class7 (ran it twice with time cmd). 
+It did not actually take 928 sec so I don't understand.  With flash_attn= True, took 945 sec according to bash cmd "time". 
+Each inference cost 34sec on the GPU on class7. Measured with  two calls to time(). 
+The cost to load the 7B model is 45sec. The model is not quantized. 
 
 I cannot run Mistral on my mac gpu using llama.cpp (I could not get it to compile). No luck with llama-cpp-python either. Mistral has another issue: it cannot be run without flash attention, which requires CUDA. Zephyr can be run with and without flash attention (there is an argument in from_pretrained(....) that controls that.
 
